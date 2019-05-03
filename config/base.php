6a8d4b52b3afa39edf35a0fdd87121fab2304f9a
SkyForest233 <50165738+SkyForest233@users.noreply.github.com>
2019-05-03 09:22:15 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,107 @@
+<?php return array (
+  'site_name' => 'SkyForest',
+  'password' => 'gao.62237595',
+  'style' => 'material',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'beVAW((qkajxFHGX48089@}',
+  'client_id' => 'a9b4d461-b866-402c-80df-84db8cebdb7e',
+  'redirect_uri' => 'https://oneindex.github.io/',
+  'refresh_token' => 'OAQABAAAAAADCoMpjJXrxTq9VG9te-7FXErb-eyqsownlGLrjSxMMERXAzzjtgFP6JTVlDlYFWiFxanRuuNC1mAGVt9aSqKn1UANtA14pZRd3P-SNkFkAkF_j0LHoZ1ad3h2zLIxHh-STMxEqjEs1dYY_hQnfjEzAtA_stQQ9jIY3LrI9BplCWyDUtQbVHz4QC4FEZtubxWpw-PKhnc8R928kJP-_EvvZQwufIqMkStOBlMJ1CNf3rgzzPJqFYugSprwhTdg0QANuTXPRGSkWVBZf34urLrdhlEjcvqgMYjDhB6lreHAzJUlMDVn3p0LJfzycVCpVmqEKPL5FNrVwMFn7yAfpJndP3RbnTZNAdYiAcHDLgfoMlsc8-sokIjUiLDQeIOawcDzAXgBijAhAWLDHVMBsDMWlKlhzt4E9JYoOQbEyhojUVD8MyNcqT7s-fgY52uw1oYvByR5Ex65UZtoYEFy3uL_kkcNY1IUSoHPhywVsdumGz7ra7CVu5DqCNsMh6C7TJb64PxC7IMM7yu6AyZDQK0b9PkKmOf2qWsYii5Fv4KqNate5dCLgJx5Nt33sPuc2_In7HKsf9Thcpgx1PGxsKaqvFd5B0rHIoM5FbdHF30So6ZpKI_7UZyq7CbvFIxT10YnZp4_khd-xgt05wQYso9Ne1bDj_SKuTjfpgw086XBArPYZUfOv-TUGmai1k6vTpSBH7S6jOZaqw77fX_E2KGlhTOtXbkc_ZKx9WoszV_H7RaAAwm4_zND_c74dr6uGBWNGEYBn9dCRRE6h8GUuKygKwmPsBOdPSAwvgz09U6n4pSAA',
+  'onedrive_hide' => '手机里的图片
+',
+  'onedrive_hotlink' => '',
+);
